38dd5a230fecd967cff306c177a7c69be8cf4142
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-22 17:42:20 -0400

    can't get custom font

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,12 @@
 export default function Header() {
     return (
         <>
-            <div className="-mt-[100vh] h-20 w-full bg-[#403f4c8c] backdrop-blur-sm text-black flex flex-row z-10">
+            <div className="-mt-[100vh] h-20 w-full bg-[#d9d8c9] text-black flex flex-row z-10 shadow-xl">
                 <div className="w-[35%] flex justify-start items-center">
 
                 </div>
                 <div className="w-[30%] flex justify-center items-center">
-                    <h1 className="font-bold text-4xl">Hack Club Journal</h1>
+                    <h1 className="text-2xl">Hack Club Journal</h1>
                 </div>
                 <div className="w-[35%] flex justify-end items-center">
                     <p className="p-5">Yap</p>
