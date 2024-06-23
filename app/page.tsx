5e63444360b5ea804b13ef009a4330454ff00fd2
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-22 20:46:26 -0400

    Added trending and recent

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Header from '../components/header';
 export default function Home() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Hero />
     </>
   )
--- components/hero.tsx
+++ components/hero.tsx
@@ -3,32 +3,54 @@ import Image from 'next/image';
 export default function Hero() {
     return (
         <>
-            <div className="h-[100vh] w-full bg-[#F9FAEF] flex justify-end items-start z-0">
-                {/* <div className="w-fit h-fit">
-                    <h1 className="font-bold text-6xl">Hack Club Journal</h1>
-                    <p className="pt-5 w-[400px] italic text-2xl">A free scientific journal for all teens.</p>
+            <div className="relative h-[50vh] w-full">
+                <Image
+                    src="/outernet.jpg"
+                    alt="Hero image"
+                    layout="fill"
+                    objectFit="cover"
+                />
+                <div className="h-[50vh] w-full backdrop-blur-lg bg-[#16161452]"></div>
+            </div>
+            <div className="absolute top-0 h-[50vh] w-full bg-none flex flex-row justify-center items-center z-0">
+                <div className="w-fit h-fit">
+                    <h1 className="font-bold text-[80px] text-[#F9FAEF]">Hack Club Journal</h1>
+                    <div className="w-fit italic text-[#F9FAEF] text-[35px]">A <div className="rotate-6 w-fit bg-[red] p-1 rounded-lg">free</div> scientific journal for all teens.</div>
                     <div className="mt-5 flex justify-center items-center w-fit h-fit gap-[20px]">
-                        <button className="bg-black border-2 border-black hover:bg-white hover:text-black text-white px-5 py-2 rounded-md">Submit Your Paper</button>
-                        <button className="border-2 hover:bg-black hover:text-white border-black px-5 py-2 rounded-md">Get Free Stickers</button>
+                        <button className="border-2 bg-[#F9FAEF] hover:bg-black hover:text-[#F9FAEF] border-black px-5 py-2 rounded-md">Get Started</button>
+                        <button className="bg-black border-2 border-black hover:bg-[#F9FAEF] hover:text-black text-[#F9FAEF] px-5 py-2 rounded-md">Get Free Stickers</button>
                     </div>
                 </div>
-                <div className="pl-[200px] w-fit h-fit">
-                    <h1 className="text-4xl font-bold">Recent papers:</h1>
-                    <div className="mt-5">
-                        <p>Insert RSS here.</p>
+                <div className="ml-[100px] h-[400px] w-[300px] bg-[#F9FAEF] wiggle rounded-lg p-3 flex flex-col">
+                    <h1 className="mb-1 text-2xl underline decoration-dotted">Trending Paper</h1>
+                    <p className="italic">Why OuterNet is so awesome</p>
+                    <p className="text-sm mb-3">- by Zach Latta</p>
+                    <div className="flex-grow relative">
+                        <Image
+                            src="/outernet.jpg"
+                            alt="Test image"
+                            layout="fill"
+                            objectFit="cover"
+                            className="rounded-lg"
+                        />
                     </div>
-                </div> */}
-                <div className="relative h-[500px] w-full fade-to-transparent">
-                    <Image
-                        src="/outernet.jpg"
-                        alt="Hero image"
-                        layout="fill"
-                        objectFit="cover"
-                        className="w-full h-[500px]"
-                    />
-                    <div className="h-[500px] w-full backdrop-blur-sm bg-[#f9faef52]"></div>
+                </div>
+                <div className="ml-[100px] h-[400px] w-[300px] bg-[#F9FAEF] wiggle rounded-lg p-3 flex flex-col">
+                    <h1 className="mb-1 text-2xl underline decoration-dotted">Recent Paper</h1>
+                    <p className="italic">Why OuterNet is so awesome</p>
+                    <p className="text-sm mb-3">- by Zach Latta</p>
+                    <div className="flex-grow relative">
+                        <Image
+                            src="/outernet.jpg"
+                            alt="Test image"
+                            layout="fill"
+                            objectFit="cover"
+                            className="rounded-lg"
+                        />
+                    </div>
                 </div>
             </div>
+            <div className="w-full h-[50vh] bg-[#F9FAEF]"></div>
         </>
     );
 }
